776dbe285877e682069ec81270ba73c3cb77d882
thomasnield <thomasnield@live.com>
2025-09-29 15:42:57 -0500

    finish section 2

---
+++ 2_using_ai_for_data_pipelines/6_fastapi.py
@@ -0,0 +1,5 @@
+# Use FastAPI to make a service to get all orders and return then in JSON format
+# Let's be very careful to not expose sensitive data in production
+# To expose our database to a service.
+# This is just a first pass at using FastAPI
+# and it may go well, or not so well!
